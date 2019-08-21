150d1ad7fe193a70dd92f161eb9e242e9a8ca435
unknown <alec.thomas.quinn@gmail.com>
2019-08-21 13:46:13 -0500

    create react app, app rendering in browser

---
+++ team-builder/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// yarn global add create-react-app
+// create-react-app team-builder
+// cd team-builder
+// yarn start
+// BEHOLD THIS
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
